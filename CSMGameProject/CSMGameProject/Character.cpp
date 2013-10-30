f14246886e30cb28f67360d9681f76ee46cf29bf
skatpgusskat <skatpgusskat@naver.com>
2013-10-30 03:34:11 -0700

    Animation 기능과 그것을 위한 FSM 상태변화 기능 넣음

--- CSMGameProject/CSMGameProject/Character.cpp
+++ CSMGameProject/CSMGameProject/Character.cpp
@@ -1,20 +1,73 @@
 #include "Character.h"
 
 
-CCharacter::CCharacter(void)
+CCharacter::CCharacter(void):m_CharacterSprite(NULL)
 {
-	m_CharacterSprite = NNSprite::Create(L"Sprite/character.png");
-	m_CharacterSprite->SetPosition( 0.f, 0.f );
-	AddChild(m_CharacterSprite);
-}
+	TransState(IDLE);
 
+	AnimationImageInfo idleAnimationImageInfo = {2, L"idle",{0.1f,0.1f}};
+	m_AnimationImageInfo.insert( std::map< CharacterState, AnimationImageInfo >::value_type(IDLE,idleAnimationImageInfo));
+	
+	AnimationImageInfo walkAnimationImageInfo = {2, L"walk",{0.2f,0.1f}};
+	m_AnimationImageInfo.insert( std::map< CharacterState, AnimationImageInfo >::value_type(WALK,walkAnimationImageInfo));
+	
+	AnimationImageInfo attackAnimationImageInfo = {2, L"attack",{0.1f,0.1f}};
+	m_AnimationImageInfo.insert( std::map< CharacterState, AnimationImageInfo >::value_type(ATTACK,attackAnimationImageInfo));
+	
+	AnimationImageInfo dieAnimationImageInfo = {2, L"die",{0.1f,0.1f}};
+	m_AnimationImageInfo.insert( std::map< CharacterState, AnimationImageInfo >::value_type(DIE,dieAnimationImageInfo));
+
+	m_AnimationFrameTime = 0.f;
+}
 
 CCharacter::~CCharacter(void)
 {
 }
 
+void CCharacter::TransState(CharacterState state)
+{
+	m_CharacterState = state;
+	m_AnimationFrameTime = 0;
+	AnimationImageInfo nowAnimationImageInfo = m_AnimationImageInfo.find(m_CharacterState)->second;
+	if( m_CharacterSprite != NULL)
+	{
+		RemoveChild(m_CharacterSprite);
+		delete m_CharacterSprite;
+	}
+	WCHAR buff[1024];
+	swprintf(buff,L"Sprite/%s_%d.png",nowAnimationImageInfo.imagePrefix,m_NowFrame);
+	std::wstring framePath = buff;
+	m_CharacterSprite = NNSprite::Create(framePath);
+	AddChild(m_CharacterSprite);
+}
+
+
 void CCharacter::Update( float dTime)
 {
+	//Animation With m_State
+	AnimationImageInfo nowAnimationImageInfo = m_AnimationImageInfo.find(m_CharacterState)->second;
+	
+	//Calculate 'Now Frame' With integration dtime
+	m_AnimationFrameTime += dTime;
+	bool isFrameChange = false;
+	while(nowAnimationImageInfo.frameDelay[m_NowFrame] < m_AnimationFrameTime)
+	{
+		m_AnimationFrameTime -= nowAnimationImageInfo.frameDelay[m_NowFrame];
+		m_NowFrame = (m_NowFrame+1)%nowAnimationImageInfo.totalFrameCount;
+		isFrameChange = true;
+	}
+	if(isFrameChange == true)
+	{
+		RemoveChild(m_CharacterSprite);
+		delete m_CharacterSprite;
+		WCHAR buff[1024];
+		swprintf(buff,L"Sprite/%s_%d.png",nowAnimationImageInfo.imagePrefix,m_NowFrame);
+		std::wstring framePath = buff;
+		m_CharacterSprite = NNSprite::Create(framePath);
+		AddChild(m_CharacterSprite);
+	}
+
+
 	//Check Moving Input, and set Position to move
 	if ( NNInputSystem::GetInstance()->GetKeyState( 'A' ) == KEY_DOWN  || 
 		NNInputSystem::GetInstance()->GetKeyState( 'A' ) == KEY_PRESSED)
@@ -40,6 +93,9 @@ void CCharacter::Update( float dTime)
 		//Down
 		SetPosition( GetPosition() + NNPoint(0.f,100.f) * dTime );
 	}
+
+
+	//Change Image By now Frame.
 }
 void CCharacter::Render()
 {
--- CSMGameProject/CSMGameProject/Character.h
+++ CSMGameProject/CSMGameProject/Character.h
@@ -2,17 +2,37 @@
 #include "NNObject.h"
 #include "NNSprite.h"
 #include "NNInputSystem.h"
+enum CharacterState
+{
+	IDLE,
+	WALK,
+	ATTACK,
+	DIE,
+};
+struct AnimationImageInfo
+{
+	int totalFrameCount;
+	std::wstring imagePrefix;
+	float frameDelay[1024];
+};
 class CCharacter :
 	public NNObject
 {
 private:
 	NNSprite* m_CharacterSprite;
+	CharacterState m_CharacterState;
+	std::map<CharacterState,AnimationImageInfo> m_AnimationImageInfo; 
+	float m_AnimationFrameTime; // init when Animation Type Change.
+	int m_NowFrame;
+
 public:
 	CCharacter(void);
 	virtual ~CCharacter(void);
+	NNCREATE_FUNC(CCharacter);
+	void TransState(CharacterState state);
 	
+private:
 	void Render();
 	void Update( float dTime );
-	NNCREATE_FUNC(CCharacter);
 };
 
