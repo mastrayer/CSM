d63db2d74b8df6ff59bf7e7fdd9942c30b0f5a98
kciter <kciter@naver.com>
2013-11-06 23:16:31 +0900

    * XMLSample Scene Insert in Main.cpp

--- CSMGameProject/CSMGameProject/Main.cpp
+++ CSMGameProject/CSMGameProject/Main.cpp
@@ -3,6 +3,7 @@
 #include "GameScene.h"
 
 #include "AnimationSample.h"
+#include "XMLSample.h"
 
 /*
 void main()
@@ -10,7 +11,8 @@ void main()
 	WinMain(0, 0, 0, 3);
 }*/
 
-int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
+//int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
+int main()
 {
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
@@ -22,7 +24,8 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdP
 	Application->Init( L"D2D Test", 800, 600, D2D );
 
 	// NNSceneDirector::GetInstance()->ChangeScene( CGameScene::Create() );
-	NNSceneDirector::GetInstance()->ChangeScene( AnimationSample::Create() );
+	// NNSceneDirector::GetInstance()->ChangeScene( AnimationSample::Create() );
+	NNSceneDirector::GetInstance()->ChangeScene( XMLSample::Create() );
 
 	Application->Run();
 	Application->Release();
