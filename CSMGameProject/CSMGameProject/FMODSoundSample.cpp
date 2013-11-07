0567f1bcc0bc1b01d366bbf30aa513b4785d1b4f
kciter <kciter@naver.com>
2013-11-07 21:20:07 +0900

    * Create FMODSoundSample Scene

---
+++ CSMGameProject/CSMGameProject/FMODSoundSample.cpp
@@ -0,0 +1,23 @@
+
+#ifdef _DEBUG
+
+#include "FMODSoundSample.h"
+
+FMODSoundSample::FMODSoundSample()
+{
+}
+FMODSoundSample::~FMODSoundSample()
+{
+
+}
+
+void FMODSoundSample::Render()
+{
+
+}
+void FMODSoundSample::Update( float dTime )
+{
+
+}
+
+#endif
