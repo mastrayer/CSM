de323375417c1ab56cc79f9d1c80a95ac899f12a
kciter <kciter@naver.com>
2013-11-09 12:54:27 +0900

    * Create ABCircularBuffer Class

---
+++ CSMGameProject/CSMGameServer/ABCircularBuffer.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include <assert.h>
+
+class ABCircularBuffer
+{
+public:
+	ABCircularBuffer();
+	~ABCircularBuffer();
+
+	char* Reserve( int size, OUT int& reserved );
+	void Commit( int size );
+
+	char* GetFirstDataBlock( OUT int& size );
+	void DecommitFirstDataBlock( int size );
+	int GetCommittedSize() const;
+	int GetReservationSize() const;
+	int GetCapacity() const;
+
+private:
+	int GetSpaceAfterA() const;
+	int GetBFreeSpace() const;
+
+private:
+	char* m_Buffer;
+	int m_IndexA;
+	int m_SizeA;
+	int m_IndexB;
+	int m_SizeB;
+	int m_Capacity;
+	int m_IndexReserve;
+	int m_SizeReserve;
+};
