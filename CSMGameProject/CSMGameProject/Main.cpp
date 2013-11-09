546d651f9c9868b9a4c4977a8bb525315e8d8d97
kciter <kciter@naver.com>
2013-11-09 10:00:06 +0900

    * Added Console Frame in Framework

--- CSMGameProject/CSMGameProject/Main.cpp
+++ CSMGameProject/CSMGameProject/Main.cpp
@@ -7,6 +7,7 @@
 #include "SpriteAtlasSample.h"
 #include "CameraSample.h"
 #include "FMODSoundSample.h"
+#include "NetworkSample.h"
 
 /*
 void main()
@@ -14,12 +15,17 @@ void main()
 	WinMain(0, 0, 0, 3);
 }*/
 
-//int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
-int main()
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
 {
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc( );
+
+	AllocConsole();
+	FILE* console;
+	freopen_s( &console, "CONOUT$", "wt", stdout );
+
+	printf_s("Console Open \n");
 #endif
 
 	NNApplication* Application = NNApplication::GetInstance();
@@ -31,10 +37,15 @@ int main()
 	// NNSceneDirector::GetInstance()->ChangeScene( XMLSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( SpriteAtlasSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( CameraSample::Create() );
-	NNSceneDirector::GetInstance()->ChangeScene( FMODSoundSample::Create() );
+	// NNSceneDirector::GetInstance()->ChangeScene( FMODSoundSample::Create() );
+	NNSceneDirector::GetInstance()->ChangeScene( NetworkSample::Create() );
 
 	Application->Run();
 	Application->Release();
+
+#ifdef _DEBUG
+	FreeConsole();
+#endif
 
 	return 0;
 }
