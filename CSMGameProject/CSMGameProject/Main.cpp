0cbe8041a7b921a18758c643592ef2941c473dce
kciter <kciter@naver.com>
2013-11-09 10:19:16 +0900

    * NetworkSystem Complate

--- CSMGameProject/CSMGameProject/Main.cpp
+++ CSMGameProject/CSMGameProject/Main.cpp
@@ -9,17 +9,11 @@
 #include "FMODSoundSample.h"
 #include "NetworkSample.h"
 
-/*
-void main()
-{
-	WinMain(0, 0, 0, 3);
-}*/
-
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
 {
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
-	//_CrtSetBreakAlloc( );
+	//_CrtSetBreakAlloc( 155 );
 
 	AllocConsole();
 	FILE* console;
