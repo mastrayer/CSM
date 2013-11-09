028e8889f585690d16a97541240cc6c4bd96ac14
kciter <kciter@naver.com>
2013-11-09 10:01:53 +0900

    * NetworkSample Test

--- CSMGameProject/CSMGameProject/NetworkSample.cpp
+++ CSMGameProject/CSMGameProject/NetworkSample.cpp
@@ -2,10 +2,14 @@
 #ifdef _DEBUG
 
 #include "NetworkSample.h"
+#include "Packet.h"
+#include "PacketFunction.h"
+#include "NNNetworkSystem.h"
 
 NetworkSample::NetworkSample()
 {
-
+	//NNNetworkSystem::GetInstance()->Init();
+	NNNetworkSystem::GetInstance()->SetPacketFunction(PKT_TEST,TestPacketFunction);
 }
 NetworkSample::~NetworkSample()
 {
