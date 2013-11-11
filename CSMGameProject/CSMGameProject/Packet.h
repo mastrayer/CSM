6147f513b41de2b72210cdd2d2c3d65d123decbd
skatpgusskat <skatpgusskat@naver.com>
2013-11-11 10:01:51 -0800

    KeyStateUpdateRequest 오타 수정 및 패킷데이터 수정

--- CSMGameProject/CSMGameProject/Packet.h
+++ CSMGameProject/CSMGameProject/Packet.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "NNPacketHeader.h"
+#include "NNInputSystem.h"
 
 #define MAX_CHAT_LEN	1024
 
@@ -49,21 +50,31 @@ struct ChatBroadcastResult : public NNPacketHeader
 	char m_Chat[MAX_CHAT_LEN] ;
 } ;
 
-struct KeyStateUpdateResult : public NNPacketHeader
+struct KeyStateUpdateRequset : public NNPacketHeader
 {
-	KeyStateUpdateResult()
+	KeyStateUpdateRequset()
 	{
-		m_Size = sizeof(KeyStateUpdateResult) ;
+		m_Size = sizeof(KeyStateUpdateRequset) ;
 		m_Type = PKT_CS_KEYSTATE ;
 		m_PlayerId = -1 ;
 		
-		memset(m_Name, 0, MAX_NAME_LEN) ;
-		memset(m_Chat, 0, MAX_CHAT_LEN) ;
+		m_UpDirectKey = NNInputSystem::GetInstance()->GetUpDirectKeyState();
+		m_DownDirectKey = NNInputSystem::GetInstance()->GetDownDirectKeyState();
+		m_LeftDirectKey = NNInputSystem::GetInstance()->GetLeftDirectKeyState();
+		m_RightDirectKey = NNInputSystem::GetInstance()->GetRightDirectKeyState();
+		m_AttackKey = NNInputSystem::GetInstance()->GetAttackKeyState();
+		m_UserActiveSkillKey = NNInputSystem::GetInstance()->GetUserActiveSkillKeyState();
+		m_TypeActiveSkillKey = NNInputSystem::GetInstance()->GetTypeActiveSkillKeyState();
 	}
 	
 	int	m_PlayerId ;
-	char m_Name[MAX_NAME_LEN] ;
-	char m_Chat[MAX_CHAT_LEN] ;
+	int m_UpDirectKey;
+	int m_DownDirectKey;
+	int m_LeftDirectKey;
+	int m_RightDirectKey;
+	int m_AttackKey;
+	int m_UserActiveSkillKey;
+	int m_TypeActiveSkillKey;
 } ;
 
 #pragma pack(pop)
