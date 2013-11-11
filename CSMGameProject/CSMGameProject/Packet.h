f12750610c807e09fd9adf472c4488ff986a0a39
skatpgusskat <skatpgusskat@naver.com>
2013-11-11 09:52:17 -0800

    패킷 폴더를 NetworkSystem으로 옮김

--- CSMGameProject/CSMGameProject/Packet.h
+++ CSMGameProject/CSMGameProject/Packet.h
@@ -14,6 +14,7 @@ enum PacketTypes
 	
 	PKT_CS_CHAT		= 3,
 	PKT_SC_CHAT		= 4,
+	PKT_CS_KEYSTATE = 5,
 };
 
 #pragma pack(push,1)
@@ -66,5 +67,21 @@ struct ChatBroadcastResult : public NNPacketHeader
 	char m_Chat[MAX_CHAT_LEN] ;
 } ;
 
+struct KeyStateUpdateResult : public NNPacketHeader
+{
+	KeyStateUpdateResult()
+	{
+		m_Size = sizeof(KeyStateUpdateResult) ;
+		m_Type = PKT_CS_KEYSTATE ;
+		m_PlayerId = -1 ;
+		
+		memset(m_Name, 0, MAX_NAME_LEN) ;
+		memset(m_Chat, 0, MAX_CHAT_LEN) ;
+	}
+	
+	int	m_PlayerId ;
+	char m_Name[MAX_NAME_LEN] ;
+	char m_Chat[MAX_CHAT_LEN] ;
+} ;
 
 #pragma pack(pop)
