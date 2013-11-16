9d34375795a469154ef1dd412707776303908c48
김지환 <mastrayer@naver.com>
2013-11-16 01:26:17 -0800

    # 사이즈 재조정

--- Animation Tool/main.Designer.cs
+++ Animation Tool/main.Designer.cs
@@ -29,77 +29,222 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.image_panel = new System.Windows.Forms.Panel();
-            this.aaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.timeline_panel = new System.Windows.Forms.Panel();
-            this.attribute_panel = new System.Windows.Forms.Panel();
-            this.work_panel = new System.Windows.Forms.Panel();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.ButtonSpriteDelete = new System.Windows.Forms.Button();
+            this.ButtonSpriteAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoadedSprite = new System.Windows.Forms.PictureBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.workSpace = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LoadedSprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aaToolStripMenuItem});
+            this.menu});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(932, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(792, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // image_panel
+            // menu
             // 
-            this.image_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.image_panel.Location = new System.Drawing.Point(0, 27);
-            this.image_panel.Name = "image_panel";
-            this.image_panel.Size = new System.Drawing.Size(189, 593);
-            this.image_panel.TabIndex = 1;
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(31, 20);
+            this.menu.Text = "aa";
             // 
-            // aaToolStripMenuItem
+            // splitContainer1
             // 
-            this.aaToolStripMenuItem.Name = "aaToolStripMenuItem";
-            this.aaToolStripMenuItem.Size = new System.Drawing.Size(31, 20);
-            this.aaToolStripMenuItem.Text = "aa";
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
-            // timeline_panel
+            // splitContainer1.Panel1
             // 
-            this.timeline_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.timeline_panel.Location = new System.Drawing.Point(195, 509);
-            this.timeline_panel.Name = "timeline_panel";
-            this.timeline_panel.Size = new System.Drawing.Size(737, 111);
-            this.timeline_panel.TabIndex = 2;
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel1MinSize = 400;
             // 
-            // attribute_panel
+            // splitContainer1.Panel2
             // 
-            this.attribute_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.attribute_panel.Location = new System.Drawing.Point(830, 27);
-            this.attribute_panel.Name = "attribute_panel";
-            this.attribute_panel.Size = new System.Drawing.Size(102, 476);
-            this.attribute_panel.TabIndex = 3;
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2MinSize = 100;
+            this.splitContainer1.Size = new System.Drawing.Size(792, 545);
+            this.splitContainer1.SplitterDistance = 441;
+            this.splitContainer1.TabIndex = 2;
             // 
-            // work_panel
+            // splitContainer2
             // 
-            this.work_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.work_panel.Location = new System.Drawing.Point(195, 27);
-            this.work_panel.Name = "work_panel";
-            this.work_panel.Size = new System.Drawing.Size(629, 476);
-            this.work_panel.TabIndex = 4;
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
             // 
-            // Form1
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.AutoScroll = true;
+            this.splitContainer2.Panel1.Controls.Add(this.ButtonSpriteDelete);
+            this.splitContainer2.Panel1.Controls.Add(this.ButtonSpriteAdd);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.LoadedSprite);
+            this.splitContainer2.Panel1MinSize = 100;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Panel2MinSize = 400;
+            this.splitContainer2.Size = new System.Drawing.Size(792, 441);
+            this.splitContainer2.SplitterDistance = 100;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // ButtonSpriteDelete
+            // 
+            this.ButtonSpriteDelete.Location = new System.Drawing.Point(75, 3);
+            this.ButtonSpriteDelete.Name = "ButtonSpriteDelete";
+            this.ButtonSpriteDelete.Size = new System.Drawing.Size(20, 20);
+            this.ButtonSpriteDelete.TabIndex = 0;
+            this.ButtonSpriteDelete.Text = "-";
+            this.ButtonSpriteDelete.UseVisualStyleBackColor = true;
+            // 
+            // ButtonSpriteAdd
+            // 
+            this.ButtonSpriteAdd.Location = new System.Drawing.Point(53, 3);
+            this.ButtonSpriteAdd.Name = "ButtonSpriteAdd";
+            this.ButtonSpriteAdd.Size = new System.Drawing.Size(20, 20);
+            this.ButtonSpriteAdd.TabIndex = 0;
+            this.ButtonSpriteAdd.Text = "+";
+            this.ButtonSpriteAdd.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sprites";
+            // 
+            // LoadedSprite
+            // 
+            this.LoadedSprite.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LoadedSprite.Location = new System.Drawing.Point(0, 0);
+            this.LoadedSprite.Name = "LoadedSprite";
+            this.LoadedSprite.Size = new System.Drawing.Size(98, 439);
+            this.LoadedSprite.TabIndex = 3;
+            this.LoadedSprite.TabStop = false;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.label4);
+            this.splitContainer3.Panel1.Controls.Add(this.workSpace);
+            this.splitContainer3.Panel1MinSize = 400;
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.label3);
+            this.splitContainer3.Panel2MinSize = 100;
+            this.splitContainer3.Size = new System.Drawing.Size(688, 441);
+            this.splitContainer3.SplitterDistance = 584;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 421);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // workSpace
+            // 
+            this.workSpace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.workSpace.Location = new System.Drawing.Point(0, 0);
+            this.workSpace.Name = "workSpace";
+            this.workSpace.Size = new System.Drawing.Size(582, 439);
+            this.workSpace.TabIndex = 0;
+            this.workSpace.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Attribute";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Timeline";
+            // 
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 621);
-            this.Controls.Add(this.work_panel);
-            this.Controls.Add(this.attribute_panel);
-            this.Controls.Add(this.timeline_panel);
-            this.Controls.Add(this.image_panel);
+            this.ClientSize = new System.Drawing.Size(792, 569);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "main";
             this.Text = "CSM Animation Tool";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LoadedSprite)).EndInit();
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            this.splitContainer3.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,11 +253,18 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.Panel image_panel;
-        private System.Windows.Forms.ToolStripMenuItem aaToolStripMenuItem;
-        private System.Windows.Forms.Panel timeline_panel;
-        private System.Windows.Forms.Panel attribute_panel;
-        private System.Windows.Forms.Panel work_panel;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonSpriteDelete;
+        private System.Windows.Forms.Button ButtonSpriteAdd;
+        private System.Windows.Forms.PictureBox workSpace;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox LoadedSprite;
     }
 }
 
--- Animation Tool/main.cs
+++ Animation Tool/main.cs
@@ -13,8 +13,11 @@ namespace Animation_Tool
     public partial class main : Form
     {
         public main()
-        {
+        {            
             InitializeComponent();
+
+            LoadedSprite.Size = new Size(1000, 3000);
+            LoadedSprite.Image = new Bitmap(1000, 2000);
         }
     }
 }
