0dff109ff8ade5b134552c1922e25f4bb7d79b1e
kciter <kciter@naver.com>
2013-11-25 21:22:54 +0900

    * last update

--- CSMGameProject/CSMGameServer/Player.cpp
+++ CSMGameProject/CSMGameServer/Player.cpp
@@ -26,33 +26,32 @@ void Player::TransState(PlayerState state)
 
 void Player::Update( float dTime)
 {
-	
-		//Move myPlayer with Game Key States.
-		//Check Moving Input, and set Position to d
-		if ( mGameKeyStates.leftDirectKey ==  KEY_DOWN  || 
-			mGameKeyStates.leftDirectKey == KEY_PRESSED )
-		{
-			//Left
-			SetPosition( GetPosition() + Point( 1.f, 0.f ) * dTime * 100.f );
-		}
-		if ( mGameKeyStates.rightDirectKey == KEY_DOWN  || 
-			mGameKeyStates.rightDirectKey == KEY_PRESSED)
-		{
-			//Right
-			SetPosition( GetPosition() + Point( -1.f, 0.f ) * dTime * 100.f );
-		}
-		if (mGameKeyStates.upDirectKey == KEY_DOWN  || 
-			mGameKeyStates.upDirectKey == KEY_PRESSED)
-		{
-			//UP
-			SetPosition( GetPosition() + Point( 0.f, -1.f ) * dTime * 100.f );
-		}
-		if ( mGameKeyStates.downDirectKey == KEY_DOWN  || 
-			mGameKeyStates.downDirectKey == KEY_PRESSED)
-		{
-			//Down
-			SetPosition( GetPosition() + Point( 0.f, 1.f ) * dTime * 100.f );
-		}
+	//Move myPlayer with Game Key States.
+	//Check Moving Input, and set Position to d
+	if ( mGameKeyStates.leftDirectKey ==  KEY_DOWN  || 
+		mGameKeyStates.leftDirectKey == KEY_PRESSED )
+	{
+		//Left
+		SetPosition( GetPosition() + Point( 1.f, 0.f ) * dTime * 100.f );
+	}
+	if ( mGameKeyStates.rightDirectKey == KEY_DOWN  || 
+		mGameKeyStates.rightDirectKey == KEY_PRESSED)
+	{
+		//Right
+		SetPosition( GetPosition() + Point( -1.f, 0.f ) * dTime * 100.f );
+	}
+	if (mGameKeyStates.upDirectKey == KEY_DOWN  || 
+		mGameKeyStates.upDirectKey == KEY_PRESSED)
+	{
+		//UP
+		SetPosition( GetPosition() + Point( 0.f, -1.f ) * dTime * 100.f );
+	}
+	if ( mGameKeyStates.downDirectKey == KEY_DOWN  || 
+		mGameKeyStates.downDirectKey == KEY_PRESSED)
+	{
+		//Down
+		SetPosition( GetPosition() + Point( 0.f, 1.f ) * dTime * 100.f );
+	}
 }
 PlayerInfo Player::GetPlayerInfo()
 {
