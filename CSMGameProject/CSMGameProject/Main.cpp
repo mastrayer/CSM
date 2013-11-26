9692dc951ff6b7bbb9d34a67f0e70e8e1b1cf83e
kciter <kciter@naver.com>
2013-11-27 02:45:48 +0900

    * Added StoryScene

--- CSMGameProject/CSMGameProject/Main.cpp
+++ CSMGameProject/CSMGameProject/Main.cpp
@@ -8,6 +8,7 @@
 #include "CameraSample.h"
 #include "FMODSoundSample.h"
 #include "NetworkSample.h"
+#include "StoryScene.h"
 
 #ifdef _DEBUG
 #pragma comment(lib, "NNGameFramework_DEBUG.lib")
@@ -32,13 +33,15 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdP
 
 	Application->Init( L"D2D Test", 800, 600, D2D );
 
-	NNSceneDirector::GetInstance()->ChangeScene( CGameScene::Create() );
+	// NNSceneDirector::GetInstance()->ChangeScene( CGameScene::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( AnimationSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( XMLSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( SpriteAtlasSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( CameraSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( FMODSoundSample::Create() );
 	// NNSceneDirector::GetInstance()->ChangeScene( NetworkSample::Create() );
+
+	NNSceneDirector::GetInstance()->ChangeScene( StoryScene::Create() );
 
 	Application->Run();
 	Application->Release();
