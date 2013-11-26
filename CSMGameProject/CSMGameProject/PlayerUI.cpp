3a6ddd1e855792b21a25d3aa236de97326e970b9
kciter <kciter@naver.com>
2013-11-27 05:00:33 +0900

    * UI Bug Fix

--- CSMGameProject/CSMGameProject/PlayerUI.cpp
+++ CSMGameProject/CSMGameProject/PlayerUI.cpp
@@ -6,7 +6,7 @@ PlayerUI::PlayerUI()
 	m_HpBar = NNSprite::Create( L"Sprite/hpbar.png" );
 	//m_HpBarFrame = NNSprite::Create( L"Sprite/hpbar_frame.png" );
 	m_HpBar->SetCenter( m_HpBar->GetImageWidth()/2.f, m_HpBar->GetImageHeight()/2.f );
-	m_HpBarFrame->SetCenter( m_HpBar->GetImageWidth()/2.f, m_HpBar->GetImageHeight()/2.f );
+	//m_HpBarFrame->SetCenter( m_HpBar->GetImageWidth()/2.f, m_HpBar->GetImageHeight()/2.f );
 	AddChild( m_HpBar );
 	//AddChild( m_HpBarFrame );
 }
