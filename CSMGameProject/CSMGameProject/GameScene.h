0da9b0a173409fa034a1b8c5e6e9ed08d02726f4
kciter <kciter@naver.com>
2013-11-27 04:15:40 +0900

    * Added GameUISet

--- CSMGameProject/CSMGameProject/GameScene.h
+++ CSMGameProject/CSMGameProject/GameScene.h
@@ -9,21 +9,12 @@
 #include "PlayerManager.h"
 #include "PacketHandler.h"
 
-#include "NNScene.h"
-#include "GameMap.h"
-#include "Player.h"
-#include "NNLabel.h"
-#include "NNInputSystem.h"
-#include "PacketType.h"
-#include "PlayerManager.h"
-#include "PacketHandler.h"
+#include "GameUISet.h"
 
 class CGameScene : public NNScene
 {
 private:
 	CGameMap* m_GameMap;
-	NNLabel* m_FPSLabel;
-	wchar_t m_FPSLabelBuff[100];
 	GameKeyStates m_NowGameKeyStates;
 	GameKeyStatesUpdateHandler* m_GameKeyStatesUpdateHandler;
 	LoginHandler* m_LoginHandler;
@@ -45,4 +36,6 @@ private:
 	bool isChangedGameKeyStates();
 	float GetNowAngle();
 	bool isChangedAngle();
+
+	void InitNetworkSetting();
 };
