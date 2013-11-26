49e69c2cf3722df69c9b92f1225e40a2df5d1f40
kciter <kciter@naver.com>
2013-11-27 02:28:36 +0900

    * Fixed Sprite File Path, Text Position

--- CSMGameProject/CSMGameProject/FirstCut.cpp
+++ CSMGameProject/CSMGameProject/FirstCut.cpp
@@ -3,13 +3,14 @@
 
 FirstCut::FirstCut()
 {
-	m_Background = NNSprite::Create( L"Resources/Texture/map.png" );
+	m_Background = NNSprite::Create( L"Sprite/map.png" );
 	AddChild( m_Background );
 
 	m_Text = NNLabel::Create( L"테스트 테스트\n테스트 테스트 입니다.", L"맑은 고딕", 20.f );
 	AddChild( m_Text );
 
-	m_Text->SetPosition( 30.f, 600.f );
+	m_Text->SetPosition( 30.f, 500.f );
+	m_Text->SetBold( true );
 }
 FirstCut::~FirstCut()
 {
