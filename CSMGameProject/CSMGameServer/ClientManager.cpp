1903661ce9d38b6f1d33922168822ae86133eb01
skatpgusskat <skatpgusskat@naver.com>
2013-11-25 16:25:32 -0800

    플레이어 시뮬레이션이 매순간 되도록

--- CSMGameProject/CSMGameServer/ClientManager.cpp
+++ CSMGameProject/CSMGameServer/ClientManager.cpp
@@ -42,8 +42,8 @@ void ClientManager::OnPeriodWork()
 		mLastGCTick = currTick ;
 	}
 
-	/// 접속된 클라이언트 세션별로 주기적으로 해줘야 하는 일 (주기는 알아서 정하면 됨 - 지금은 1초로 ㅎㅎ)
-	if ( currTick - mLastClientWorkTick >= 1000 )
+	/// 접속된 클라이언트 세션별로 주기적으로 해줘야 하는 일 (주기는 알아서 정하면 됨 - 지금은 0초로 ㅎㅎ)
+	if ( currTick - mLastClientWorkTick >= 0 )
 	{
 		ClientPeriodWork() ;
 		mLastClientWorkTick = currTick ;
