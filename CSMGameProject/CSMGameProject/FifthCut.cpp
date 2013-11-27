57192c9576fb2f4e7e0fde6a5856d5751083e7dd
mooneegee <mooneegee@naver.com>
2013-11-27 20:20:51 +0900

    @ Fifth StoryScene Added

---
+++ CSMGameProject/CSMGameProject/FifthCut.cpp
@@ -0,0 +1,32 @@
+#include "FifthCut.h"
+#include "NNApplication.h"
+
+FifthCut::FifthCut()
+{
+	float width = (float)NNApplication::GetInstance()->GetScreenWidth();
+	float height = (float)NNApplication::GetInstance()->GetScreenHeight();
+
+	m_Background = NNSprite::Create( L"Sprite/StoryScene_4.png" );
+	m_Background->SetPosition( width/2, height/2 );
+	m_Background->SetCenter( m_Background->GetImageWidth()/2.f, m_Background->GetImageHeight()/2.f );
+	AddChild( m_Background );
+
+	m_Text = NNLabel::Create( L"악의 정령왕 신틸리케를 무찔러주세요. 일어나세요, 용사여...", L"맑은 고딕", 20.f );
+	AddChild( m_Text );
+
+	m_Text->SetPosition( 30.f, 500.f );
+	m_Text->SetBold( true );
+}
+FifthCut::~FifthCut()
+{
+
+}
+
+void FifthCut::Render()
+{
+	StoryCut::Render();
+}
+void FifthCut::Update( float dTime )
+{
+	StoryCut::Update( dTime );
+}
