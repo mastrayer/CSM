0a2c67665719da2555e6a633a9d0614b7676d066
mooneegee <mooneegee@naver.com>
2013-11-27 20:23:24 +0900

    @ ThirdCut.h Added

---
+++ CSMGameProject/CSMGameProject/ThirdCut.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "StoryCut.h"
+#include "NNSprite.h"
+#include "NNLabel.h"
+
+class ThirdCut : public StoryCut
+{
+public:
+	ThirdCut();
+	virtual ~ThirdCut();
+
+	void Render();
+	void Update( float dTime );
+
+	NNCREATE_FUNC(ThirdCut);
+private:
+	NNSprite* m_Background;
+	NNLabel* m_Text;
+};
