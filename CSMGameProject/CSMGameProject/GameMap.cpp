ce66a4e544874541316f23d5747ac1604938f5e2
jihwankim <mastrayer@nhnnext.org>
2013-11-29 13:10:49 +0900

    # Tile 깔기

--- CSMGameProject/CSMGameProject/GameMap.cpp
+++ CSMGameProject/CSMGameProject/GameMap.cpp
@@ -1,10 +1,24 @@
 #include "GameMap.h"
+#include"iostream"
 
-CGameMap::CGameMap(void)
+CGameMap::CGameMap(std::wstring path)
 {
-	m_MapSprite = NNSprite::Create( L"Sprite/map.png" );
-	m_MapSprite->SetPosition( 0.f, 0.f );
-	AddChild( m_MapSprite );
+	NNSpriteAtlas *temp;
+	for(int i=0;i<12; ++i)
+	{
+		for(int j=0; j<12; ++j)
+		{
+			temp = NNSpriteAtlas::Create(L"C:/Users/kim jihwan/Desktop/temp-tile/ground.png");
+			AddChild(temp);
+
+			temp->SetCutSize(0,0,64.f,64.f);
+			temp->SetPosition(i*64,j*64);
+		}
+	}
+// 	convertFileToMap(path);
+// 	m_MapSprite = NNSprite::Create( L"Sprite/map.png" );
+// 	m_MapSprite->SetPosition( 0.f, 0.f );
+// 	AddChild( m_MapSprite );
 }
 
 CGameMap::~CGameMap(void)
@@ -17,6 +31,91 @@ void CGameMap::Update( float dTime )
 }
 
 void CGameMap::Render()
+{
+	NNObject::Render();
+}
+ 
+void CGameMap::CreateMapInfo(NNXML* xml)
+{
+	/*
+	std::string tile;
+	int usedTileSet = 0;
+
+	// MapInfo
+	{
+		// Map Size
+		{
+			m_MapInfo.mapWidth = atoi(xml->XPathToString("/map/mapInfo/size/@width").c_str());
+			m_MapInfo.mapHeight = atoi(xml->XPathToString("/map/mapInfo/size/@height").c_str());
+		}
+		// Used Tile Set
+		{
+			usedTileSet = atoi(xml->XPathToString("/map/mapInfo/usedTileSet/@count").c_str());
+		}
+	}
+
+	// TILE Initialize //////////////////////////////////////////////////////////////////////////
+
+	m_MapInfo.tile = new TILE *[m_MapInfo.mapHeight];
+	for(int i=0;i<m_MapInfo.mapHeight; ++i)
+		m_MapInfo.tile[i] = new TILE[m_MapInfo.mapWidth];
+
+	/////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Tile Info
+	{
+		for(int i=0; i<m_MapInfo.mapHeight; ++i)
+		{
+			for(int j=0; j<m_MapInfo.mapWidth; ++j)
+			{
+				tile = "t" + std::to_string(i) + "-" + std::to_string(j);
+
+				m_MapInfo.tile[i][j].isFull = xml->XPathToString("/map/tileInfo/" + tile + "/isFull").c_str() == "true" ? true : false;
+				{
+					m_MapInfo.tile[i][j].isFull = xml->XPathToString("/map/tileInfo/" + tile + "/TileImageInfo/@Index").c_str();
+				}
+			}
+		}
+	}
+
+	
+	
+
+	getchar();*/
+}
+void CGameMap::convertFileToMap( std::wstring path )
+{
+	std::wstring FILENAME[2] = { L"map.xml", L"TileSet" };
+	NNZip *loadZip;
+
+	// XML LOAD
+	loadZip = NNResourceManager::GetInstance()->UnzipFileToMemory(path, FILENAME[0]);
+	m_MapXMLData= NNResourceManager::GetInstance()->LoadXMLFromMemory(loadZip);
+
+	CreateMapInfo(m_MapXMLData);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////                            /////////////////////////////
+/////////////////////////////           CTile            /////////////////////////////
+/////////////////////////////                            /////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+CTile::CTile()
+	: m_attribute(0), m_height(0), m_image(nullptr), m_isFull(false)
+{
+
+}
+CTile::~CTile()
+{
+
+}
+void CTile::Update( float dTime )
+{
+	NNObject::Update( dTime );
+}
+
+void CTile::Render()
 {
 	NNObject::Render();
 }
--- CSMGameProject/CSMGameProject/GameMap.h
+++ CSMGameProject/CSMGameProject/GameMap.h
@@ -3,18 +3,57 @@
 
 #include "NNObject.h"
 #include "NNSprite.h"
+#include "NNResourceManager.h"
+#include "NNSpriteAtlas.h"
+
+#define ATTRIBUTE_MOVE 0x01
+
+class CTile : public NNObject
+{
+public:
+	CTile();
+	virtual ~CTile();
+
+	void Render();
+	void Update( float dTime );
+
+	NNCREATE_FUNC(CTile);
+
+//private:
+	NNSpriteAtlas *m_image;
+	int m_attribute;
+	int m_height;
+	bool m_isFull;
+};
+struct MapInfo
+{
+	int mapWidth, mapHeight;
+	CTile **tile;
+	NNSprite *image;
+};
 
 class CGameMap : public NNObject
 {
 public:
-	CGameMap(void);
+	CGameMap(std::wstring path);
 	virtual ~CGameMap(void);
 	
 	void Render();
 	void Update( float dTime );
 
-	NNCREATE_FUNC(CGameMap);
+	void CreateMapInfo(NNXML* xml);
+	void convertFileToMap(std::wstring path);
+
+	static CGameMap* Create(std::wstring path)
+	{
+		CGameMap* pInstance = new CGameMap(path);
+		return pInstance;
+	}
+	//NNCREATE_FUNC(CGameMap);
+
 
 private:
 	NNSprite* m_MapSprite;
+	NNXML* m_MapXMLData;
+	MapInfo m_MapInfo;
 };
