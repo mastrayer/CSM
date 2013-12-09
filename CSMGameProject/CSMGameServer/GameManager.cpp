b42637227c852c62ee6876c1ca17a6a2304dc2d0
skatpgusskat <skatpgusskat@naver.com>
2013-12-09 14:05:16 -0800

    GameManager를 통해서 데스매치에 필요한 기능, 패킷 추가.

---
+++ CSMGameProject/CSMGameServer/GameManager.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "GameManager.h"
+#include "ClientManager.h"
+#include "PacketType.h"
+
+GameManager::GameManager(int killLimit):mKillLimit(killLimit), mKillScore(), mPlayerCount()
+{
+}
+
+
+GameManager::~GameManager(void)
+{
+}
+
+void GameManager::DiePlayer(int team)
+{
+	mKillScore[team] ++;
+	if(mKillScore[team] > mKillLimit)
+	{
+		EndOfGame(team);
+	}
+}
+
+int GameManager::GiveTeamNumber()
+{
+	//딱히 밸런스 알고리즘은 없음
+	if(mPlayerCount[1] < mPlayerCount[0])
+	{
+		mPlayerCount[1]++;
+		return 1;
+	}
+	mPlayerCount[0]++;
+	return 0;
+}
+
+void GameManager::LogOutPlayer(int team)
+{
+	mPlayerCount[team]--;
+}
+
+void GameManager::EndOfGame(int winnerTeam)
+{
+	EndOfGameResult outPacket = EndOfGameResult();
+	outPacket.mWinnerTeam = winnerTeam;
+	GClientManager->BroadcastPacket(nullptr,&outPacket);
+	mKillLimit = 1024*1024;
+	mKillScore[0] = mKillScore[1] = -1024*1024;
+}
+
+GameManager* GGameManager = nullptr;
