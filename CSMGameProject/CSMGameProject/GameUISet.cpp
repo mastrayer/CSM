8434bec725631073e8c45a1d834f9d4c14c176eb
mooneegee <mooneegee@naver.com>
2013-12-10 21:54:25 +0900

    @ GameUI TeamKillScore bar Added

--- CSMGameProject/CSMGameProject/GameUISet.cpp
+++ CSMGameProject/CSMGameProject/GameUISet.cpp
@@ -4,24 +4,27 @@
 
 GameUISet::GameUISet()
 {
-	mHp = 80;
 	mSkillCooltime[0] = 5.f;
 	mSkillCooltime[1] = 5.f;
+	//mKillCount = 25;
+	//mRedKillCount = 40;
 	swprintf_s(mSkillCooltimeBuff[0], L"%d", (int)mSkillCooltime[0]);
 	swprintf_s(mSkillCooltimeBuff[1], L"%d", (int)mSkillCooltime[1]);
 
 	float width = (float)NNApplication::GetInstance()->GetScreenWidth();
 	float height = (float)NNApplication::GetInstance()->GetScreenHeight();
 
-	mFPSLabel = NNLabel::Create( L"", L"¸¼Àº °íµñ", 20 );
-	AddChild( mFPSLabel );
+	mFPSLabel = NNLabel::Create(L"", L"¸¼Àº °íµñ", 20);
+	AddChild(mFPSLabel);
 
 	mCharacterUIFrame = NNSprite::Create(L"Sprite/CharacterUIFrame.png");
 	mCharacterUIFrame->SetPosition(0.f, 450.f);
+	mCharacterUIFrame->SetOpacity(0.8f);
 
 	mSkillUIFrame = NNSprite::Create(L"Sprite/SkillUIFrame.png");
 	mSkillUIFrame->SetCenter(mSkillUIFrame->GetImageWidth() / 2.f, mSkillUIFrame->GetImageHeight() / 2.f);
 	mSkillUIFrame->SetPosition(width / 2.f, height / 2.f + 250);
+	mSkillUIFrame->SetOpacity(0.8f);
 
 	mHpBarBackground = NNSprite::Create(L"Sprite/GameHpBar_BackGround.png");
 	mHpBarBackground->SetCenter(mHpBarBackground->GetImageWidth() / 2.f, mHpBarBackground->GetImageHeight() / 2.f);
@@ -29,7 +32,19 @@ GameUISet::GameUISet()
 
 	mHpBar = NNSprite::Create(L"Sprite/GameHpBar.png");
 	mHpBar->SetPosition(340.f, 570.f);
-	
+
+	mCrown = NNSprite::Create(L"Sprite/Crown.png");
+	mCrown->SetCenter(mCrown->GetImageWidth() / 2.f, mCrown->GetImageHeight() / 2.f);
+	mCrown->SetPosition(width / 2.f, 30);
+
+	mKillBar[BLUE] = NNSprite::Create(L"Sprite/KillPointBlueBar.png");
+	mKillBar[BLUE]->SetPosition(width / 2.f - 32, 20);
+	mKillBar[BLUE]->SetScaleX(-1.f);
+
+	mKillBar[RED] = NNSprite::Create(L"Sprite/KillPointRedBar.png");
+	mKillBar[RED]->SetPosition(width / 2.f + 32, 20);
+	//mRedKillBar->SetScaleX(-1.f);
+
 	mSkillUI[0] = NNSprite::Create(L"Sprite/TypeSkillUI_0.png");
 	mSkillUI[0]->SetCenter(mSkillUI[0]->GetImageWidth() / 2.f, mSkillUI[0]->GetImageHeight() / 2.f);
 	mSkillUI[0]->SetPosition(width / 2.f - 32, height / 2.f + 235);
@@ -37,7 +52,7 @@ GameUISet::GameUISet()
 	mSkillUI[1] = NNSprite::Create(L"Sprite/UserSkillUI_0.png");
 	mSkillUI[1]->SetCenter(mSkillUI[1]->GetImageWidth() / 2.f, mSkillUI[1]->GetImageHeight() / 2.f);
 	mSkillUI[1]->SetPosition(width / 2.f + 32, height / 2.f + 235);
-	
+
 	mSkillTimer[0] = NNLabel::Create(L"", L"¸¼Àº °íµñ", 40.f);
 	mSkillTimer[0]->SetCenter(mSkillUI[0]->GetCenterX(), mSkillUI[0]->GetCenterY());
 	mSkillTimer[0]->SetPosition(width / 2.f - 35, height / 2.f + 250);
@@ -47,20 +62,36 @@ GameUISet::GameUISet()
 	mSkillTimer[1]->SetCenter(mSkillUI[1]->GetCenterX(), mSkillUI[1]->GetCenterY());
 	mSkillTimer[1]->SetPosition(width / 2.f + 35, height / 2.f + 250);
 	mSkillTimer[1]->SetRGBA(255, 255, 255, 255);
-	
+
+	mKillPoint[BLUE] = NNLabel::Create(L"40", L"¸¼Àº °íµñ", 20.f);
+	mKillPoint[BLUE]->SetCenter(mKillPoint[BLUE]->GetCenterX(), mKillPoint[BLUE]->GetCenterY());
+	mKillPoint[BLUE]->SetPosition(width / 2.f - 250 - 20, 20);
+	mKillPoint[BLUE]->SetRGBA(255, 255, 255, 255);
+
+	mKillPoint[RED] = NNLabel::Create(L"25", L"¸¼Àº °íµñ", 20.f);
+	mKillPoint[RED]->SetCenter(mKillPoint[RED]->GetCenterX(), mKillPoint[RED]->GetCenterY());
+	mKillPoint[RED]->SetPosition(width / 2.f + 250, 20);
+	mKillPoint[RED]->SetRGBA(255, 255, 255, 255);
+	//mRedKillPointNumber->SetScaleX(-1.f);
+
 	AddChild(mCharacterUIFrame);
 	AddChild(mSkillUIFrame);
 	AddChild(mHpBarBackground);
 	AddChild(mHpBar);
+	AddChild(mKillBar[BLUE]);
+	AddChild(mKillBar[RED]);
+	AddChild(mCrown);
 	AddChild(mSkillUI[0]);
 	AddChild(mSkillUI[1]);
 	AddChild(mSkillTimer[0]);
 	AddChild(mSkillTimer[1]);
-	
-	mCharacterFace = NNSprite::Create( L"Sprite/BlueSlime.png" );
+	AddChild(mKillPoint[BLUE]);
+	AddChild(mKillPoint[RED]);
+
+	/*mCharacterFace = NNSprite::Create( L"Sprite/BlueSlime.png" );
 	mCharacterFace->SetCenter( mCharacterFace->GetImageWidth()/2.f, mCharacterFace->GetImageHeight()/2.f );
 	mCharacterFace->SetPosition( 60.f, 60.f );
-	AddChild( mCharacterFace );
+	AddChild( mCharacterFace );*/
 	
 	mMyPlayer = CPlayerManager::GetInstance()->GetMyPlayer();
 	ZeroMemory( mFPSLabelBuff, sizeof(mFPSLabelBuff) );
@@ -74,14 +105,18 @@ void GameUISet::Render()
 {
 	NNUISet::Render();
 }
-void GameUISet::Update( float dTime )
+void GameUISet::Update(float dTime)
 {
-	NNUISet::Update( dTime );
+	NNUISet::Update(dTime);
 
-	swprintf_s( mFPSLabelBuff, L"%d", (int)NNApplication::GetInstance()->GetFPS() );
+	swprintf_s(mFPSLabelBuff, L"%d", (int)NNApplication::GetInstance()->GetFPS());
 
-	mFPSLabel->SetString( mFPSLabelBuff );
-	mHpBar->SetScale( mHp/100.f, 1.f );
+	mFPSLabel->SetString(mFPSLabelBuff);
+	mHpBar->SetScale(mMyPlayer->GetPlayerHP() / 50.f, 1.f);
+	//mKillBar[BLUE]->SetScale(CPlayerManager::GetInstance()->GetKillScore(BLUE) / 50.f, 1.f);
+	mKillBar[BLUE]->SetScale(25 / 50.f, 1.f);
+	//mKillBar[RED]->SetScale(CPlayerManager::GetInstance()->GetKillScore(RED) / 50.f, 1.f);
+	mKillBar[RED]->SetScale(40 / 50.f, 1.f);
 
 	for (int i = 0; i < SKILL_COUNT; ++i)
 		ControlSkillUI((PlayerState)(TYPE_ACTIVE_SKILL + i), dTime);
--- CSMGameProject/CSMGameProject/GameUISet.h
+++ CSMGameProject/CSMGameProject/GameUISet.h
@@ -8,6 +8,15 @@
 #include "PlayerManager.h"
 #include "EffectManager.h"
 
+/*
+enum KILLPOINT
+{
+	BLUE = 0,
+	RED = 1,
+};
+*/
+
+
 class GameUISet : public NNUISet
 {
 public:
@@ -41,12 +50,15 @@ private:
 	NNSprite *mCharacterFace;
 	NNSprite *mHpBar;
 	NNSprite *mHpBarBackground;
+	NNSprite *mKillBar[2];
+	
+	NNSprite *mCrown;
 
-	int mHp;
+	NNLabel *mKillPoint[2];
+	
 	CPlayer *mMyPlayer;
 	wchar_t mFPSLabelBuff[100];
 	wchar_t mSkillCooltimeBuff[2][5];
-	//int mKillCount;
-
+	
 	friend class Player;
 };
