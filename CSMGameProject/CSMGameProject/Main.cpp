0687f342b358c50d10886ae86f0cfb531f744db3
skatpgusskat <skatpgusskat@naver.com>
2013-12-09 21:15:13 -0800

    샘플 삭제 및 오타 수정

--- CSMGameProject/CSMGameProject/Main.cpp
+++ CSMGameProject/CSMGameProject/Main.cpp
@@ -13,7 +13,7 @@
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nShowCmd )
 {
 #ifdef _DEBUG
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEmDF|_CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc( 155 );
 
 	AllocConsole();
