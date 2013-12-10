ddb1967a12f1451b7cc8d94fce5dbc958f1a1fae
kciter <kciter@naver.com>
2013-12-11 04:47:02 +0900

    * StoryCut Refactoring

--- CSMGameProject/CSMGameProject/FirstCut.cpp
+++ CSMGameProject/CSMGameProject/FirstCut.cpp
@@ -4,24 +4,27 @@
 
 FirstCut::FirstCut()
 {
+	////////// Window Frame Size //////////
 	float width = (float)NNApplication::GetInstance()->GetScreenWidth();
 	float height = (float)NNApplication::GetInstance()->GetScreenHeight();
 
+	////////// Background Image //////////
 	mBackground = NNSprite::Create( L"Sprite/StoryScene_0.png" );
 	mBackground->SetPosition( width/2, height/2 );
 	mBackground->SetCenter( mBackground->GetImageWidth()/2.f, mBackground->GetImageHeight()/2.f );
 	AddChild( mBackground );
 
+	////////// Story Text Box //////////
 	mStoryTextBox = NNSprite::Create( L"Sprite/StoryTextBox.png");
 	mStoryTextBox->SetPosition( width/2, height/2 + 200.f );
 	mStoryTextBox->SetCenter( mStoryTextBox->GetImageWidth()/2.f, mStoryTextBox->GetImageHeight()/2.f );
 	AddChild( mStoryTextBox );
 
+	////////// Story Text Label //////////
 	mText = NNLabel::Create( L"수많은 정령들이 해피해피하게 사네요.", L"맑은 고딕", 20.f );
-	AddChild( mText );
-
 	mText->SetPosition( 100.f, 460.f );
 	mText->SetBold( true );
+	AddChild( mText );
 }
 FirstCut::~FirstCut()
 {
