12e59779e734ca82bfac228b888e548defe13b72
kciter <kciter@naver.com>
2013-12-18 04:03:32 +0900

    * Added Player Type Change PacketProcess

---
+++ CSMGameProject/CSMGameServer/PlayerState.cpp
@@ -0,0 +1,61 @@
+
+#include "stdafx.h"
+#include "PlayerState.h"
+#include "PlayerManager.h"
+
+void PlayerTypeChangeA( ClientSession* client, PacketHeader* header, CircularBuffer* buffer )
+{
+	TypeChangeToARequest inPacket ;
+	buffer->Read((char*)&inPacket, header->mSize);
+
+	Player* _player = GPlayerManager->GetPlayer(inPacket.mPlayerId);
+	_player->ChangeType( TYPE_A );
+
+	TypeChangeToAResult outPacket;
+	outPacket.mPlayerId = inPacket.mPlayerId;
+	
+	if( !client->Broadcast(&outPacket) )
+		return;
+}
+void PlayerTypeChangeB( ClientSession* client, PacketHeader* header, CircularBuffer* buffer )
+{
+	TypeChangeToBRequest inPacket ;
+	buffer->Read((char*)&inPacket, header->mSize);
+
+	Player* _player = GPlayerManager->GetPlayer(inPacket.mPlayerId);
+	_player->ChangeType( TYPE_B );
+
+	TypeChangeToBResult outPacket;
+	outPacket.mPlayerId = inPacket.mPlayerId;
+
+	if( !client->Broadcast(&outPacket) )
+		return;
+}
+void PlayerTypeChangeC( ClientSession* client, PacketHeader* header, CircularBuffer* buffer )
+{
+	TypeChangeToCRequest inPacket ;
+	buffer->Read((char*)&inPacket, header->mSize);
+
+	Player* _player = GPlayerManager->GetPlayer(inPacket.mPlayerId);
+	_player->ChangeType( TYPE_D );
+
+	TypeChangeToCResult outPacket;
+	outPacket.mPlayerId = inPacket.mPlayerId;
+
+	if( !client->Broadcast(&outPacket) )
+		return;
+}
+void PlayerTypeChangeD( ClientSession* client, PacketHeader* header, CircularBuffer* buffer )
+{
+	TypeChangeToDRequest inPacket ;
+	buffer->Read((char*)&inPacket, header->mSize);
+
+	Player* _player = GPlayerManager->GetPlayer(inPacket.mPlayerId);
+	_player->ChangeType( TYPE_A );
+
+	TypeChangeToDResult outPacket;
+	outPacket.mPlayerId = inPacket.mPlayerId;
+
+	if( !client->Broadcast(&outPacket) )
+		return;
+}
