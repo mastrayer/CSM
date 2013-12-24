1733f0f7587d9034830a1ee41dd5b894b11b4852
= <kciter@naver.com>
2013-12-25 00:43:16 +0900

    * Added EmailField

--- src/CSMWebServer/app/user/forms.py
+++ src/CSMWebServer/app/user/forms.py
@@ -1,13 +1,14 @@
 from flask.ext.wtf import Form, RecaptchaField
-from wtforms import TextField, PasswordField, BooleanField
+from flask.ext.wtf.html5 import EmailField
+from wtforms import TextField, PasswordField
 from wtforms.validators import Required, EqualTo, Email
 
 class LoginForm(Form):
-	email = TextField('Email address', [Required(), Email()])
+	email = EmailField('Email address', [Required(), Email()])
 	password = PasswordField('Password', [Required()])
 
 class RegisterForm(Form):
-	email = TextField('Email address', [Required(), Email()])
+	email = EmailField('Email address', [Required(), Email()])
 	password = PasswordField('Password', [Required()])
 	confirm = PasswordField('Repeat Pasword', [Required(), EqualTo('password', message='Passwords must match')])
 	nickname = TextField('NickName', [Required()])
