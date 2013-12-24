e1a9b0cb3055e6fdb69cb2b78b6c4783efeb5678
= <kciter@naver.com>
2013-12-25 04:10:17 +0900

    * Added UserForms, UserModels

--- src/CSMWebServer/app/general/views.py
+++ src/CSMWebServer/app/general/views.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 
 from app import db
-
+from app.user.forms import LoginForm
+from app.user.models import User
 
 mod = Blueprint('general', __name__, url_prefix='/')
 
 @mod.route('/')
 def main():
-	return render_template('general/main.html')
+	form = LoginForm(request.form)
+	return render_template('general/main.html', user=g.user, form=form)
 
-
+@mod.before_request
+def before_request():
+	g.user = None
+	if 'user_id' in session:
+		g.user = User.query.get(session['user_id'])
