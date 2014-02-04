f8b918b09debc4d845c60afbb339e2c18d46e358
root <root@king2.(none)>
2014-02-04 22:36:41 +0900

    * WebServer Update

--- src/CSMWebServer/run.py
+++ src/CSMWebServer/run.py
@@ -1,4 +1,8 @@
 from app import app
+from werkzeug.contrib.fixers import ProxyFix
 
-app.run(host='0.0.0.0', debug=True)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 #app.run(debug=True)
